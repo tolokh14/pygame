f9989e41c48ceef18d3ecc3e0fba7ee28628beae
tolokh14 <dzulsuyufi@gmail.com>
2022-09-29 15:48:39 +0700

    added title, icon and background color

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ pg.init()
 #create the screen
 screen = pg.display.set_mode((800,600))
 
+#title and icon
+pg.display.set_caption("Space Invaders")
+icon = pg.image.load('assets/spaceship.png')
+pg.display.set_icon(icon)
+
+#game loop
 running = True
 while running:
     for event in pg.event.get():
@@ -14,3 +20,6 @@ while running:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 running = False
+    
+    screen.fill((0,0,0))
+    pg.display.update()
