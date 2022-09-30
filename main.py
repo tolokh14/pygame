9afc543399a82df1532d8817bd10b087000c8554
tolokh14 <dzulsuyufi@gmail.com>
2022-09-30 10:22:08 +0700

    added space background

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ pg.init()
 #create the screen
 screen = pg.display.set_mode((800,600))
 
+#background
+background = pg.image.load('assets/background.png')
+
 #title and icon
 pg.display.set_caption("Space Invaders")
 icon = pg.image.load('assets/spaceship.png')
@@ -23,8 +26,8 @@ playerY_change = 0
 enemyImg = pg.image.load('assets/alien.png')
 enemyX = ran.randint(0,736)
 enemyY = 40
-enemyX_change = 0
-enemyY_change = 0
+enemyX_change = 4
+enemyY_change = 40
 
 def player(x,y):
     screen.blit(playerImg, (x, y))
@@ -37,6 +40,8 @@ running = True
 while running:
     
     screen.fill((0,0,0))
+    #background image
+    screen.blit(background, (0,0))
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -48,13 +53,13 @@ while running:
         #check keyboard press
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pg.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
             if event.key == pg.K_UP:
-                playerY_change = -0.3
+                playerY_change = -5
             if event.key == pg.K_DOWN:
-                playerY_change = 0.3
+                playerY_change = 5
         if event.type == pg.KEYUP:
             if event.key == pg.K_LEFT or event.key == pg.K_RIGHT:
                 playerX_change = 0
@@ -64,15 +69,25 @@ while running:
     playerX += playerX_change
     playerY += playerY_change
 
+    # check the boundaries
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
-
     if playerY <= 0:
         playerY = 0
     elif playerY >= 536:
         playerY = 536
+
+    #enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 4
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -4
+        enemyY += enemyY_change
 
     player(playerX,playerY)
     enemy(enemyX,enemyY)
